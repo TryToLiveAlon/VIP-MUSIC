d314476eec0eaef3aa2b77f851a1a4b7acfe34d9
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-07-19 22:42:07 +0530

    Update ping.py

--- VIPMUSIC/plugins/tools/ping.py
+++ VIPMUSIC/plugins/tools/ping.py
@@ -111,11 +111,11 @@ async def ping_com(client, message: Message, _):
                 [
                     InlineKeyboardButton(
                         text="✦ ɢʀᴏᴜᴘ ✦",
-                        url=f"https://t.me/TG_FRIENDSS",
+                        url=f"https://t.me/TryToLiveAlon",
                     ),
                     InlineKeyboardButton(
                         text="✧ ᴍᴏʀᴇ ✧",
-                        url=f"https://t.me/VIP_CREATORS",
+                        url=f"https://t.me/deathchatting_world",
                     ),
                 ],
                 [
